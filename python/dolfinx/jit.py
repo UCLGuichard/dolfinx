f9893cab6d0eba1d6c8d6b4f3b0106fb7d660e07
Chris Richardson <chris@bpi.cam.ac.uk>
2020-01-27 13:46:56 +0000

    Experiment by increasing compile time

--- python/dolfinx/jit.py
+++ python/dolfinx/jit.py
@@ -113,14 +113,14 @@ def ffc_jit(ufl_object, form_compiler_parameters=None):
 
     # Switch on type and compile, returning cffi object
     if isinstance(ufl_object, ufl.Form):
-        r = ffc.codegeneration.jit.compile_forms([ufl_object], parameters=p, cache_dir=cache_dir, **cffi_options)
+        r = ffc.codegeneration.jit.compile_forms([ufl_object], parameters=p, timeout=20, cache_dir=cache_dir, **cffi_options)
     elif isinstance(ufl_object, ufl.FiniteElementBase):
-        r = ffc.codegeneration.jit.compile_elements([ufl_object], parameters=p, cache_dir=cache_dir, **cffi_options)
+        r = ffc.codegeneration.jit.compile_elements([ufl_object], parameters=p, timeout=20, cache_dir=cache_dir, **cffi_options)
     elif isinstance(ufl_object, ufl.Mesh):
         r = ffc.codegeneration.jit.compile_coordinate_maps(
-            [ufl_object], parameters=p, cache_dir=cache_dir, **cffi_options)
+            [ufl_object], parameters=p, timeout=20, cache_dir=cache_dir, **cffi_options)
     elif isinstance(ufl_object, tuple) and isinstance(ufl_object[0], ufl.core.expr.Expr):
-        r = ffc.codegeneration.jit.compile_expressions([ufl_object], parameters=p, cache_dir=cache_dir, **cffi_options)
+        r = ffc.codegeneration.jit.compile_expressions([ufl_object], parameters=p, timeout=20, cache_dir=cache_dir, **cffi_options)
     else:
         raise TypeError(type(ufl_object))
 
